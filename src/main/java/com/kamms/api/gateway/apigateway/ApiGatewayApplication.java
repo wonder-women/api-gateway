9ddb59f726b4a1a7828c25d299c0365eb308770c
Prapurna <pmanda1@sapient.com>
2018-09-02 11:38:17 +0530

    Initial commit

---
+++ src/main/java/com/kamms/api/gateway/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.kamms.api.gateway.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
